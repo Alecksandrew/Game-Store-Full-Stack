e71bef96807dfdecd6d07369b36146c515daf9f6
Alecksandrew <alecksandrewcontato@gmail.com>
2025-08-23 14:47:16 -0300

    RegisterForm was refactored becoming cleaner

--- frontend/src/features/auth/components/RegisterForm.tsx
+++ frontend/src/features/auth/components/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, useFormContext } from "react-hook-form";
 import { Input } from "@/global/components/Input";
 import Button from "@/global/components/Button";
 import { useRegister } from "../hooks/useAuth";
@@ -6,66 +6,74 @@ import { useRegister } from "../hooks/useAuth";
 import { type RegisterFormProps } from "../types/RegisterFormType";
 import { type RegisterFormData } from "../types/RegisterFormType";
 import FormHeader from "@/global/components/FormHeader";
+import Form from "@/global/components/Form";
 
 export default function RegisterForm({ className }: RegisterFormProps) {
-  const { register, handleSubmit, formState, getValues } =
-    useForm<RegisterFormData>();
-   const { execute, isLoading, warningComponent } = useRegister();
+  const { execute, isLoading, warningComponent } = useRegister();
 
+  const ConfirmPasswordField = () => {
+    const { getValues } = useFormContext();
+    return (
+      <Input
+        label="Confirm Password"
+        name="confirmPassword"
+        type="password"
+        placeholder="Confirm your password"
+        rules={{
+          required: "Please confirm your password",
+          validate: (value) =>
+            value === getValues("password") || "The passwords do not match",
+        }}
+      />
+    );
+  };
   return (
     <>
       {warningComponent}
-      <form
-        onSubmit={handleSubmit(execute)}
-        className={`form ${className}`}
+      <Form<RegisterFormData>
+        onSubmit={execute}
+        submitText="Create account"
+        isLoading={isLoading}
       >
-        <FormHeader title="Create account" subTitle="Sign up to start your gaming journey"/>
-        <Input
-          title="User Name"
-          type="text"
-          placeholder="Enter your nickname"
-          {...register("username", { required: "User name is required" })}
-          errorMessage={formState.errors.username?.message}
+        <FormHeader
+          title="Create account"
+          subTitle="Sign up to start your gaming journey"
         />
         <Input
-          title="Email"
+          label="User Name"
+          name="username"
+          type="text"
+          placeholder="Enter your nickname"
+          rules={{ required: "User name is required" }}
+        />
+        <Input
+          label="Email"
+          name="email"
           type="email"
           placeholder="Enter your email"
-          {...register("email", {
+          rules={{
             required: "Email is required",
             pattern: {
               value: /^\S+@\S+\.\S+$/,
               message: "Please enter a valid email address",
             },
-          })}
-          errorMessage={formState.errors.email?.message}
+          }}
         />
         <Input
-          title="Password"
+          label="Password"
+          name="password"
           type="password"
           placeholder="Create a password"
-          {...register("password", {
+          rules={{
             required: "Password is required",
             minLength: {
               value: 8,
               message: "Password must be at least 8 characters long",
             },
-          })}
-          errorMessage={formState.errors.password?.message}
+          }}
         />
-        <Input
-          title="Confirm Password"
-          type="password"
-          placeholder="Confirm your password"
-          {...register("confirmPassword", {
-            required: "Please confirm your password",
-            validate: (value) =>
-              value === getValues("password") || "The passwords do not match",
-          })}
-          errorMessage={formState.errors.confirmPassword?.message}
-        />
-        <Button title="Create account" type="submit" disabled={isLoading}  />
-      </form>
+        <ConfirmPasswordField />
+      </Form>
     </>
   );
 }
