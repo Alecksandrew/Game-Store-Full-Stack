e08c475a93436917b35b6b3d1400193c6337ca48
Alecksandrew <alecksandrewcontato@gmail.com>
2025-08-30 14:23:23 -0300

    SimilarGamesSection ADDED

--- frontend/src/features/Details/components/SimilarGamesSection.tsx
+++ frontend/src/features/Details/components/SimilarGamesSection.tsx
@@ -1,24 +1,22 @@
 import { useContext } from "react";
 import { GameDetailsDataContext } from "../contexts/GameDetailsDataContext";
-import {GameCardNoPrice} from "@/features/Home/components/GameCard/GameCardPresets";
+import { GameCardNoPrice } from "@/features/Home/components/GameCard/GameCardPresets";
 
 export default function SimilarGamesSection() {
   const data = useContext(GameDetailsDataContext);
 
   function listSimilarGames() {
-    return data.similarGames.slice(0, 4).map((similarGame) => {
-      return (
-        <GameCardNoPrice
-          gameData={similarGame}
-          className="min-h-[400px] text-2xl"
-        />
-      );
+    return data.similarGames.slice(0, 3).map((similarGame) => {
+      return <GameCardNoPrice gameData={similarGame} />;
     });
   }
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-2 w-full">
-      {listSimilarGames()}
+    <div>
+      <h2 className="text-text-primary text-3xl">Similar games</h2>
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(220px,1fr))] gap-2 w-full mt-3">
+        {listSimilarGames()}
+      </div>
     </div>
   );
 }
