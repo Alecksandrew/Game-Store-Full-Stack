776e8deeeded8602ee6e4e7c7ed491bb29bb2ea1
Alecksandrew <alecksandrewcontato@gmail.com>
2025-10-02 18:23:31 -0300

    CartPage component created

--- frontend/src/features/Cart/pages/CartPage.tsx
+++ frontend/src/features/Cart/pages/CartPage.tsx
@@ -1,24 +1,49 @@
-import CartItem from "../components/CartItem";
-import OrderSummary from "../components/OrderSummary";
+import { useContext } from 'react';
+import Header from '@/features/Home/components/Header';
+import { CartContext } from '../context/CartContext';
+import CartItem from '../components/CartItem';
+import OrderSummary from '../components/OrderSummary';
+import { Link } from 'react-router';
+import { MdOutlineShoppingCart } from "react-icons/md";
 
 
+export default function CartPage() {
+  const { cartItems } = useContext(CartContext);
 
+  return (
+    <>
+      <Header />
+      <div className="bg-bg-primary min-h-screen py-10">
+        <div className="w-9/10 max-w-[1200px] mx-auto">
+          <h1 className="text-4xl text-text-primary font-orbitron mb-6">My Cart</h1>
 
-export default function CartPage(){
-    const gameDataMockTest = {
-    id: 1,
-    name: "Starline Odyssey",
-    coverUrl: "https://picsum.photos/seed/starline/400/560",
-    price: 59.99,
-    discountPrice: 29.99
-  };
+          {cartItems.length === 0 ? (
+            <div className="text-center text-text-secondary py-20 rounded-lg">
+              <MdOutlineShoppingCart className='mx-auto mb-4' size={"3rem"}/>
+              <p className="text-2xl mb-4">Your cart is empty.</p>
+              <Link to="/" className="text-primary hover:underline font-bold text-lg">
+                Continue Shopping
+              </Link>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
+            
+              <div className="lg:col-span-2">
+                <ul className="space-y-4">
+                  {cartItems.map((game) => (
+                    <CartItem key={game.id} game={game} />
+                  ))}
+                </ul>
+              </div>
 
-
-    return <>
-    <ul>
-        <CartItem game={gameDataMockTest}/>
-    </ul>
-    <OrderSummary/>
-    
+             
+              <div className="lg:col-span-1">
+                <OrderSummary />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </>
+  );
 }
