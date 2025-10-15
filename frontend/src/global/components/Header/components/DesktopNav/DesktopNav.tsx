a3aab699d63520df4e23da18e88523a24213a81c
Alecksandrew <alecksandrewcontato@gmail.com>
2025-10-15 15:00:09 -0300

    Header links will only show up if the user has the right permission

--- frontend/src/global/components/Header/components/DesktopNav/DesktopNav.tsx
+++ frontend/src/global/components/Header/components/DesktopNav/DesktopNav.tsx
@@ -1,14 +1,33 @@
+import { MyAccountContext } from "@/features/myAccount/context/MyAccountContext";
 import { AuthButton } from "@/global/components/AuthButton";
 import { navLinks } from "@/global/constants/navigation";
+import { useContext } from "react";
 import { Link } from "react-router";
 
 export function DesktopNav() {
   const linkClass =
     "hover:bg-bg-secondary hover:text-text-primary text-center rounded flex items-center justify-center p-1.5";
+  const {myAccountData, isLoggedIn} = useContext(MyAccountContext);
 
+ const filteredLinks = navLinks.filter(link => {
+   
+    if (!link.authRequired) {
+      return true;
+    }
+    
+    if (!isLoggedIn) {
+      return false;
+    }
+  
+    if (link.allowedRoles && !link.allowedRoles.includes(myAccountData?.role ?? '')) {
+      return false;
+    }
+    
+    return true;
+  });    
   return (
     <nav className="text-text-primary flex justify-center items-stretch gap-5 relative max-w-[600px] mx-auto h-full">
-      {navLinks.map((link) => (
+      {filteredLinks.map((link) => (
         <Link key={link.title} className={`${linkClass}`} to={link.to}>
           {link.title}
         </Link>
--- frontend/src/global/constants/navigation.ts
+++ frontend/src/global/constants/navigation.ts
@@ -1,9 +1,21 @@
 import { PAGE_ROUTES } from "./FRONTEND_URL";
 
-export const navLinks = [
-    { title: "Home", to: PAGE_ROUTES.STORE.HOME },
-    { title: "Wishlist", to: PAGE_ROUTES.STORE.WISHLIST },
-    { title: "Cart", to: PAGE_ROUTES.STORE.CART },
-    { title: "Admin", to: PAGE_ROUTES.ADMIN.DASHBOARD },
-    { title: "My Account", to: PAGE_ROUTES.ACCOUNT },
-  ];
+export type NavLink = {
+  title: string;
+  to: string;
+  authRequired?: boolean;     // O usuário precisa estar logado?
+  allowedRoles?: string[];    // Quais roles podem ver este link?
+};
+
+export const navLinks: NavLink[] = [
+  { title: "Home", to: PAGE_ROUTES.STORE.HOME },
+  { title: "Wishlist", to: PAGE_ROUTES.STORE.WISHLIST, authRequired: true },
+  { title: "Cart", to: PAGE_ROUTES.STORE.CART },
+  { title: "My Account", to: PAGE_ROUTES.ACCOUNT, authRequired: true },
+  {
+    title: "Admin",
+    to: PAGE_ROUTES.ADMIN.DASHBOARD,
+    authRequired: true,
+    allowedRoles: ["Admin"],
+  }
+]
