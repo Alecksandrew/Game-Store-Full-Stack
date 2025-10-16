dbe7ded660a26513aff387e80f30b7125a39d75f
Alecksandrew <alecksandrewcontato@gmail.com>
2025-10-16 20:57:52 -0300

    update

--- GameStoreAPI/Program.cs
+++ GameStoreAPI/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173", "https://localhost:5173", "https://game-store-full-stack.vercel.app/") 
+                          policy.WithOrigins("http://localhost:5173", "https://localhost:5173", "https://game-store-full-stack.vercel.app") 
                                 .AllowAnyMethod()
                                 .AllowAnyHeader();
                       });
