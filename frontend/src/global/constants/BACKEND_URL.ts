dedcd1dbc8da507ca381d74cdf377ea4dbb7c446
Alecksandrew <alecksandrewcontato@gmail.com>
2025-10-20 12:09:55 -0300

    fix: Fixed local and development enviroment
    - Fixed all local and development URLs
    - Now it is possible to keep working in local machine
    and everything in production gonna keep working

--- frontend/src/global/constants/BACKEND_URL.ts
+++ frontend/src/global/constants/BACKEND_URL.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://game-store-api-hkesgmaaexecbjcz.brazilsouth-01.azurewebsites.net";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const API_PATHS = {
   AUTH: "/api/Auth",
